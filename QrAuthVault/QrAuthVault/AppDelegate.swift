a817957b8388d7a059f64401578aab9aef565eee
Valeriy Chevtaev <myltik@gmail.com>
2015-09-12 21:43:55 +0100

    Add Cleanroom logger as static library (not as git module). Add Chrome Extension template.

--- QrAuthVault/QrAuthVault/AppDelegate.swift
+++ QrAuthVault/QrAuthVault/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import CleanroomLogger
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- QrAuthVault/QrAuthVault/ViewController.swift
+++ QrAuthVault/QrAuthVault/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CleanroomLogger
 
 class ViewController: UIViewController {
 
