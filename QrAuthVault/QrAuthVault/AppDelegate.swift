803bbe149e1b228efe701aba0bcc17b6e47e2332
Valeriy Chevtaev <myltik@gmail.com>
2015-12-18 14:17:23 +0000

    Encrypt local database in The Vault app

--- QrAuthVault/QrAuthVault/AppDelegate.swift
+++ QrAuthVault/QrAuthVault/AppDelegate.swift
@@ -66,7 +66,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("QrAuthVault.sqlite")
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType,
+                configuration: nil, URL: url,
+                options: [NSPersistentStoreFileProtectionKey: NSFileProtectionComplete])
         } catch {
             // Report any error we got.
             var dict = [String: AnyObject]()
